3bf0263f1ee53adb0b4620ab066a78c387bc32de
ChasevanHekken <cvh1717@gmail.com>
2018-09-28 02:03:01 -0400

    creating basic table and inserting record from query params

--- source/RewardsService/rewardsservice/handlers/create_handler.py
+++ source/RewardsService/rewardsservice/handlers/create_handler.py
@@ -4,13 +4,17 @@ import tornado.web
 from pymongo import MongoClient
 from tornado.gen import coroutine
 
-
 class CreateHandler(tornado.web.RequestHandler):
 
     @coroutine
     def get(self):
         client = MongoClient("mongodb", 27017)
-        db = client["Rewards"]
+        db = client["Users"]
+        users = list(db.users.find({}, {"_id": 0}))
+
         email = self.get_argument("email")
         total = self.get_argument("total")
-        self.write(email + total)
+
+        db.users.insert({"email": email, "total": total})
+
+        self.write(json.dumps(users))
