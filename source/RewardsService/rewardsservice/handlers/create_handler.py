7fd7a159561f756e0273a822dc0271bf6284053f
ChasevanHekken <cvh1717@gmail.com>
2018-09-28 16:41:22 -0400

    change rewards service create to post request

--- source/RewardsService/rewardsservice/handlers/create_handler.py
+++ source/RewardsService/rewardsservice/handlers/create_handler.py
@@ -7,9 +7,15 @@ from bson.json_util import loads
 from bson.json_util import dumps
 
 class CreateHandler(tornado.web.RequestHandler):
-    # http://localhost:7050/create?emailAddress=bob@gmail.com&orderTotal=220
+    # http://localhost:7050/create
+    # {
+    #     "emailAddress": "chase@gmail.com", 
+    #     "orderTotal": "500",
+    # }
+    # POST request
+
     @coroutine
-    def get(self):
+    def post(self):
         emailAddress = self.get_argument("emailAddress")
         orderTotal = self.get_argument("orderTotal")
         rewardsPoints = int(float(orderTotal))
