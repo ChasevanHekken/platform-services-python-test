fb69a126ce1bcf85d4174a9d2f1fdeadd92b059d
ChasevanHekken <cvh1717@gmail.com>
2018-09-28 03:36:43 -0400

    got rewards tier working

--- source/RewardsService/rewardsservice/handlers/create_handler.py
+++ source/RewardsService/rewardsservice/handlers/create_handler.py
@@ -17,16 +17,30 @@ class CreateHandler(tornado.web.RequestHandler):
 
         db = client["Users"]
         # db.users.remove()
-        users = list(db.users.find({}, {"_id": 0}))
 
         emailAddress = self.get_argument("emailAddress")
         orderTotal = self.get_argument("orderTotal")
+        rewardsPoints = int(float(orderTotal))
+        
+        rewardLevel = None
 
+        for reward in rewards:
+            if rewardsPoints >= reward.get("points"):
+                rewardLevel = reward
+            elif rewardsPoints < reward.get("points"):
+                break
+            
         user_data = {
             "emailAddress": emailAddress, 
-            "rewardsPoints": int(float(orderTotal))
+            "rewardsPoints": rewardsPoints,
+            "rewardsTier": rewardLevel.get("tier"),
+            "rewardsTierName": "name",
+            "nextRewardsTier": "next",
+            "nextRewardsTierName": "next name",
+            "nextRewadsTierProgress": "progress"
         }
 
         db.users.insert(user_data)
+        users = list(db.users.find({}, {"_id": 0}))
         chase = db.users.find({"emailAddress": "joe"})
-        self.write(dumps(chase))
+        self.write(json.dumps(users))
