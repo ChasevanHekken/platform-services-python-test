18e49546c8a164f40e4e52ca4defaac0eddcdab1
ChasevanHekken <cvh1717@gmail.com>
2018-10-01 09:41:13 -0400

    implement search by email in ui

--- source/RewardsUI/rewards/views.py
+++ source/RewardsUI/rewards/views.py
@@ -14,10 +14,20 @@ class RewardsView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
-        self.refresh(context)
-        user_form = UserForm()
-        context['user_form'] = user_form
 
+        search = request.GET.get('search', None)
+        if search != None:
+            single_user = requests.get("http://rewardsservice:7050/find?emailAddress=" + search)
+            if single_user.json():
+                context['users_data'] = [single_user.json()]
+            else:
+                self.refresh_users(context)
+        else:
+            self.refresh_users(context)
+
+        self.refresh_rewards(context)
+        self.refresh_create_form(context)
+        
         return TemplateResponse(
             request,
             self.template_name,
@@ -35,7 +45,9 @@ class RewardsView(TemplateView):
             "orderTotal": form.cleaned_data["order"]
         }
         requests.post("http://rewardsservice:7050/create", data = request_body)
-        self.refresh(context)
+        self.refresh_rewards(context)
+        self.refresh_users(context)
+        self.refresh_create_form(context)
 
         return TemplateResponse(
             request,
@@ -43,12 +55,14 @@ class RewardsView(TemplateView):
             context
         )
 
-    def refresh(self, context):
-        response = requests.get("http://rewardsservice:7050/rewards")
-        context['rewards_data'] = response.json()
+    def refresh_rewards(self, context):
+        rewards = requests.get("http://rewardsservice:7050/rewards")
+        context['rewards_data'] = rewards.json()
 
-        usersResponse = requests.get("http://rewardsservice:7050/users")
-        context['users_data'] = usersResponse.json()
+    def refresh_users(self, context):
+        all_users = requests.get("http://rewardsservice:7050/users")
+        context['users_data'] = all_users.json()
 
+    def refresh_create_form(self, context):
         user_form = UserForm()
         context['user_form'] = user_form
